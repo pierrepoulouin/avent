584b16b9145d4f651bf7a5c6ea267079de413bf9
pierrepoulouin <pierrepoulouin@gmail.com>
2018-11-06 11:37:58 +0100

    ManyToOne //

--- avent/src/main/java/co/simplon/avent/model/Link.java
+++ avent/src/main/java/co/simplon/avent/model/Link.java
@@ -8,7 +8,7 @@ import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToOne;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Link {
